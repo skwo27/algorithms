fc59072969d8e4ed03b59ca540d355916851c764
skwo27 <165741912+skwo27@users.noreply.github.com>
2024-09-25 19:58:45 +0900

    [Silver IV] Title: 스택, Time: 380 ms, Memory: 112064 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,24 @@
+steck = []
+for i in range(int(input())):
+    a = str(input())
+
+    if a.split()[0] == "push":
+        steck.append(int(a.split()[1]))
+    elif a == "pop":
+        if steck ==[]:
+            print(-1)
+        else:
+            print(steck[-1])
+            del steck[-1]
+    elif a == "size":
+        print(len(steck))
+    elif a == "empty":
+        if steck == []:
+            print(1)
+        else:
+            print(0)
+    elif a == "top":
+        if steck == []:
+            print(-1)
+        else:
+            print(steck[-1])
