11e9bf40881eaa17d335e3030bc186131036b7e6
skwo27 <165741912+skwo27@users.noreply.github.com>
2024-09-27 20:01:18 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,16 @@
+for i in range(int(input())):
+    m = str(input())
+    if m == 'Algorithm':
+        print(204)
+    elif m == 'DataAnalysis':
+        print(207)
+    elif m == 'ArtificialIntelligence':
+        print(302)
+    elif m == 'CyberSecurity':
+        print("B101")
+    elif m == 'Network':
+        print(303)
+    elif m == 'Startup':
+        print(501)
+    elif m == 'TestStrategy':
+        print(105)
