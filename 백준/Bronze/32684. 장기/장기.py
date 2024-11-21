cc36420f90d0e796707cd56e7b57416bf9b25e5b
skwo27 <165741912+skwo27@users.noreply.github.com>
2024-11-21 20:21:12 +0900

    [Bronze IV] Title: 장기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/32684. 장기/장기.py
@@ -0,0 +1,8 @@
+a,b,c,d,e,f = map(int,input().split())
+a1,b1,c1,d1,e1,f1 = map(int,input().split())
+su = a*13 + b*7 + c*5 + d*3 + e*3 +f*2
+su2 = a1*13 + b1*7 + c1*5 + d1*3 + e1*3 +f1*2+1.5
+if su>su2:
+   print('cocjr0208')
+else:
+   print("ekwoo")
